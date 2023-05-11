30c3e68dc7e78383e13f492484bba4518379d11e
Leon Klein <lellobaka@gmail.com>
2023-05-11 12:50:17 +0200

    Recensuoni
    ASDASD

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+function openReviews() {
+
+    let r = document.getElementById("container-reviews");
+    let b = document.getElementById("showRates");
+    r.classList.toggle("open");
+    r.classList.toggle("close");
+    b.classList.toggle("ruotate");
+}
